dd4037814750733efcf3c76dca2dab8fdcf2b587
DreamFog20 <thaidat@011003.com>
2024-09-22 21:37:03 +0700

    Added my project

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import profileapi.model.User;
+import service.UserService;
+
+import java.util.Optional;
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // API xem profile
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // API cập nhật profile
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+}
